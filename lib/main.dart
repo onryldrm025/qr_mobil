668804b9bf74c8d6b3965c5843183a6eff3b52f2
Onur yildirim <onryldrm025@hotmail.com>
2022-01-14 17:06:22 +0300

    change

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:qr_mobil/qr_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(const MyApp());
@@ -29,133 +30,208 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final _formKey = GlobalKey<FormState>();
+
   TextEditingController scNo = TextEditingController();
   TextEditingController name = TextEditingController();
+  TextEditingController company = TextEditingController();
+  String? nameP;
+  String? scNoP;
+  String? companyP;
+  SharedPreferences? prefs;
+
+  @override
+  void initState() {
+    getData();
+    super.initState();
+  }
+
+  getData() async {
+    prefs = await SharedPreferences.getInstance();
+    setState(() {
+      nameP = prefs?.getString('name');
+      scNoP = prefs?.getString('scNo');
+      companyP = prefs?.getString('company');
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(
-        resizeToAvoidBottomInset: false,
-        appBar: AppBar(
-          title: const Text('Qr Oluşturma'),
-          titleTextStyle: TextStyle(color: Colors.white, fontSize: 25),
-          centerTitle: true,
-        ),
-        backgroundColor: Colors.black,
-        body: Center(
-          child: Form(
-            key: _formKey,
-            child: Column(
-              children: [
-                Container(
-                  margin: EdgeInsets.all(25),
-                  decoration: BoxDecoration(
-                    color: Colors.grey.shade600,
-                    borderRadius: BorderRadius.circular(33),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Image.asset(
-                      'assets/blue_full_WH.png',
-                      width: MediaQuery.of(context).size.width * 80,
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                ),
-                Container(
-                  width: MediaQuery.of(context).size.width * 0.5,
-                  height: 35,
-                  decoration: BoxDecoration(
-                    color: Colors.grey.shade600,
-                    borderRadius: BorderRadius.circular(33),
-                  ),
-                  child: const Center(
-                    child: Text(
-                      'Hoş geldiniz.',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextFormField(
-                    controller: scNo,
-                    validator: (value) {
-                      if (value?.length != 5 || value!.isEmpty) {
-                        return 'Sicil no eksik veya yanlış!';
-                      }
-                      return null;
-                    },
-                    cursorColor: Colors.grey,
-                    style: const TextStyle(color: Colors.white),
-                    autovalidateMode: AutovalidateMode.onUserInteraction,
-                    decoration: const InputDecoration(
-                      hintStyle: TextStyle(color: Colors.white),
-                      hintText: 'Sicil No  ',
-                      border: OutlineInputBorder(),
-                      errorBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.red, width: 2),
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.grey, width: 2),
-                      ),
-                    ),
-                    keyboardType: TextInputType.number,
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextFormField(
-                    cursorColor: Colors.grey,
-                    autovalidateMode: AutovalidateMode.onUserInteraction,
-                    controller: name,
-                    style: const TextStyle(color: Colors.white),
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        return 'İsim ve soyisim';
-                      }
-                      return null;
-                    },
-                    decoration: const InputDecoration(
-                      hintStyle: TextStyle(color: Colors.white),
-                      hintText: 'Ad Soyad  ',
-                      border: OutlineInputBorder(),
-                      errorBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.red, width: 2),
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.grey, width: 2),
-                      ),
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 16.0),
-                  child: ElevatedButton(
-                    onPressed: () {
-                      if (_formKey.currentState!.validate()) {
-                        // ScaffoldMessenger.of(context).showSnackBar(
-                        //   const SnackBar(content: Text('Processing Data')),
-                        // );
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => QrScreen(
-                              name: name.text,
-                              scNo: scNo.text,
+      child: scNoP == null
+          ? Scaffold(
+              // resizeToAvoidBottomInset: false,
+              appBar: AppBar(
+                title: const Text('Qr Oluşturma'),
+                titleTextStyle:
+                    const TextStyle(color: Colors.white, fontSize: 25),
+                centerTitle: true,
+              ),
+              backgroundColor: Colors.black,
+              body: SingleChildScrollView(
+                child: Center(
+                  child: Form(
+                    key: _formKey,
+                    child: Column(
+                      children: [
+                        Container(
+                          margin: const EdgeInsets.all(25),
+                          decoration: BoxDecoration(
+                            color: Colors.grey.shade600,
+                            borderRadius: BorderRadius.circular(33),
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Image.asset(
+                              'assets/blue_full_WH.png',
+                              width: MediaQuery.of(context).size.width * 80,
+                              fit: BoxFit.cover,
                             ),
                           ),
-                        );
-                      }
-                    },
-                    child: const Text('Oluştur'),
+                        ),
+                        Container(
+                          margin: const EdgeInsets.only(bottom: 25),
+                          width: MediaQuery.of(context).size.width * 0.5,
+                          height: MediaQuery.of(context).size.height * 0.05,
+                          decoration: BoxDecoration(
+                            color: Colors.grey.shade600,
+                            borderRadius: BorderRadius.circular(33),
+                          ),
+                          child: Center(
+                            child: Text(
+                              'Hoş geldiniz.',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize:
+                                    MediaQuery.of(context).size.height * 0.03,
+                              ),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: TextFormField(
+                            controller: company,
+                            validator: (value) {
+                              if (value!.isEmpty) {
+                                return 'Şirket ismi!';
+                              }
+                              return null;
+                            },
+                            textCapitalization: TextCapitalization.characters,
+                            cursorColor: Colors.grey,
+                            style: const TextStyle(color: Colors.white),
+                            autovalidateMode:
+                                AutovalidateMode.onUserInteraction,
+                            decoration: const InputDecoration(
+                              hintStyle: TextStyle(color: Colors.white),
+                              hintText: 'Şirket İsmi  ',
+                              border: OutlineInputBorder(),
+                              errorBorder: OutlineInputBorder(
+                                borderSide:
+                                    BorderSide(color: Colors.red, width: 2),
+                              ),
+                              enabledBorder: OutlineInputBorder(
+                                borderSide:
+                                    BorderSide(color: Colors.grey, width: 2),
+                              ),
+                            ),
+                            keyboardType: TextInputType.text,
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: TextFormField(
+                            controller: scNo,
+                            validator: (value) {
+                              if (value?.length != 5 || value!.isEmpty) {
+                                return 'Sicil no eksik veya yanlış!';
+                              }
+                              return null;
+                            },
+                            cursorColor: Colors.grey,
+                            style: const TextStyle(color: Colors.white),
+                            autovalidateMode:
+                                AutovalidateMode.onUserInteraction,
+                            decoration: const InputDecoration(
+                              hintStyle: TextStyle(color: Colors.white),
+                              hintText: 'Sicil No  ',
+                              border: OutlineInputBorder(),
+                              errorBorder: OutlineInputBorder(
+                                borderSide:
+                                    BorderSide(color: Colors.red, width: 2),
+                              ),
+                              enabledBorder: OutlineInputBorder(
+                                borderSide:
+                                    BorderSide(color: Colors.grey, width: 2),
+                              ),
+                            ),
+                            keyboardType: TextInputType.number,
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: TextFormField(
+                            cursorColor: Colors.grey,
+                            autovalidateMode:
+                                AutovalidateMode.onUserInteraction,
+                            controller: name,
+                            style: const TextStyle(color: Colors.white),
+                            validator: (value) {
+                              if (value!.isEmpty) {
+                                return 'İsim ve soyisim';
+                              }
+                              return null;
+                            },
+                            decoration: const InputDecoration(
+                              hintStyle: TextStyle(color: Colors.white),
+                              hintText: 'Ad Soyad  ',
+                              border: OutlineInputBorder(),
+                              errorBorder: OutlineInputBorder(
+                                borderSide:
+                                    BorderSide(color: Colors.red, width: 2),
+                              ),
+                              enabledBorder: OutlineInputBorder(
+                                borderSide:
+                                    BorderSide(color: Colors.grey, width: 2),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 16.0),
+                          child: ElevatedButton(
+                            onPressed: () {
+                              if (_formKey.currentState!.validate()) {
+                                prefs?.setString('name', name.text);
+                                prefs?.setString('scNo', scNo.text);
+                                prefs?.setString('company', company.text);
+
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => QrScreen(
+                                      name: name.text,
+                                      scNo: scNo.text,
+                                      sirket: company.text,
+                                    ),
+                                  ),
+                                );
+                              }
+                            },
+                            child: const Text('Qr Oluştur'),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-              ],
+              ),
+            )
+          : QrScreen(
+              name: nameP!,
+              scNo: scNoP!,
+              sirket: companyP!,
             ),
-          ),
-        ),
-      ),
     );
   }
 }
--- lib/qr_screen.dart
+++ lib/qr_screen.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:qr_flutter/qr_flutter.dart';
+import 'package:qr_mobil/main.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class QrScreen extends StatefulWidget {
-  const QrScreen({Key? key, required this.scNo, required this.name})
+  const QrScreen(
+      {Key? key, required this.scNo, required this.name, required this.sirket})
       : super(key: key);
   final String scNo;
   final String name;
+  final String sirket;
   @override
   _QrScreenState createState() => _QrScreenState();
 }
@@ -14,13 +18,28 @@ class _QrScreenState extends State<QrScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       appBar: AppBar(
         title: const Text('Qr'),
+        actions: [
+          IconButton(
+              onPressed: () async {
+                SharedPreferences prefs = await SharedPreferences.getInstance();
+                prefs.clear();
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const MyHomePage()),
+                );
+              },
+              icon: const Icon(Icons.delete))
+        ],
         centerTitle: true,
       ),
       body: Center(
         child: QrImage(
-          data: "{name:${widget.name},sicilNo:${widget.scNo}}",
+          backgroundColor: Colors.white,
+          data:
+              '{"sicilismi":"${widget.name}","sicilkodu":"${widget.scNo}","sirketkodu": "$widget.sirket"}',
           version: QrVersions.auto,
           size: MediaQuery.of(context).size.width * 0.8,
         ),
